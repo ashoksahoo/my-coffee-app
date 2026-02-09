d7a682a0604d59646c0ee1febe9e7c7d513cf7af
Ashok Sahoo <hello@ashok.io>
2026-02-09 17:12:24 +0530

    feat(02-01): wire Beans tab as first tab in MainTabView
    - Beans tab with leaf icon added as first tab (most frequently accessed entity)
    - Tab order: Beans, Methods, Grinders, Settings
    - Full navigation: BeanListView -> AddBeanView (sheet) -> BeanDetailView (push)

--- CoffeeJournal/Views/MainTabView.swift
+++ CoffeeJournal/Views/MainTabView.swift
@@ -4,6 +4,13 @@ struct MainTabView: View {
     var body: some View {
         TabView {
             NavigationStack {
+                BeanListView()
+            }
+            .tabItem {
+                Label("Beans", systemImage: "leaf")
+            }
+
+            NavigationStack {
                 MethodListView()
             }
             .tabItem {
